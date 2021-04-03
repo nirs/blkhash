2859f77de105fdaf682609304f6c545739ab3f5d
Nir Soffer <nsoffer@redhat.com>
2021-04-03 22:04:45 +0300

    Add zero support
    Add blkhash_zero() for hashing zero range when contents of the data is
    known. Instead of reading the data, the caller can hash entire extent.
    For example hashing 1g of zeroes:
    
        blkhash_zero(h, 1073741824);
    
    This is is several orders of magnitude faster compared with reading data
    and detecting zeroes.

--- blkhash_test.c
+++ blkhash_test.c
@@ -60,13 +60,17 @@ void checksum(struct extent *extents, unsigned int len,
     for (int i = 0; i < len; i++) {
         struct extent *e = &extents[i];
 
-        unsigned char *buf = malloc(e->len);
-        assert(buf != NULL);
-        memset(buf, e->byte, e->len);
+        if (e->byte == '-') {
+            blkhash_zero(h, e->len);
+        } else {
+            unsigned char *buf = malloc(e->len);
+            assert(buf != NULL);
+            memset(buf, e->byte, e->len);
 
-        blkhash_update(h, buf, e->len);
+            blkhash_update(h, buf, e->len);
 
-        free(buf);
+            free(buf);
+        }
     }
 
     blkhash_final(h, md, &md_len);
@@ -95,6 +99,16 @@ void test_block_data_zero()
     TEST_ASSERT_EQUAL_STRING("cb29e710160d736b446eba4fc235ce3dc0ea5542", hexdigest);
 }
 
+void test_block_zero()
+{
+    struct extent extents[] = {
+        {'-', block_size},
+    };
+    char hexdigest[hexdigest_len];
+    checksum(extents, ARRAY_SIZE(extents), block_size, digest_name, hexdigest);
+    TEST_ASSERT_EQUAL_STRING("cb29e710160d736b446eba4fc235ce3dc0ea5542", hexdigest);
+}
+
 void test_partial_block_data()
 {
     struct extent extents[] = {
@@ -113,6 +127,47 @@ void test_partial_block_data_zero()
     char hexdigest[hexdigest_len];
     checksum(extents, ARRAY_SIZE(extents), block_size, digest_name, hexdigest);
     TEST_ASSERT_EQUAL_STRING("f3181c76c4a5094b6fd65ec1d186cb1ec35bcfa6", hexdigest);
+}
+
+void test_partial_block_zero()
+{
+    struct extent extents[] = {
+        {'-', block_size / 2},
+    };
+    char hexdigest[hexdigest_len];
+    checksum(extents, ARRAY_SIZE(extents), block_size, digest_name, hexdigest);
+    TEST_ASSERT_EQUAL_STRING("f3181c76c4a5094b6fd65ec1d186cb1ec35bcfa6", hexdigest);
+}
+
+void test_sparse()
+{
+    struct extent extents[] = {
+        {'-', block_size * 3},
+    };
+    char hexdigest[hexdigest_len];
+    checksum(extents, ARRAY_SIZE(extents), block_size, digest_name, hexdigest);
+    TEST_ASSERT_EQUAL_STRING("abf5dc2d9aba57fd01b38403c16d3d0dbf9bd341", hexdigest);
+}
+
+void test_sparse_unaligned()
+{
+    struct extent extents[] = {
+        {'-', block_size * 2},
+        {'-', block_size / 2},
+    };
+    char hexdigest[hexdigest_len];
+    checksum(extents, ARRAY_SIZE(extents), block_size, digest_name, hexdigest);
+    TEST_ASSERT_EQUAL_STRING("bb54c3bea2b1e6aedbeed5270e910fc96c26bec6", hexdigest);
+}
+
+void test_sparse_large()
+{
+    struct extent extents[] = {
+        {'-', 1024 * 1024 * 1024},
+    };
+    char hexdigest[hexdigest_len];
+    checksum(extents, ARRAY_SIZE(extents), block_size, digest_name, hexdigest);
+    TEST_ASSERT_EQUAL_STRING("fe623c97a1b52b9263043bf11e9ec686dab34da9", hexdigest);
 }
 
 void test_zero()
@@ -165,21 +220,79 @@ void test_full_unaligned()
     TEST_ASSERT_EQUAL_STRING("cb2c09df8d1e976768307c63cc764b2fb52679f8", hexdigest);
 }
 
+void test_mix()
+{
+    struct extent extents[] = {
+        /* Add pending data... */
+        {'A', block_size / 2},
+        /* Add zeroes converting zeroes to data and consume pending. */
+        {'-', block_size / 2},
+        /* Add pending zeroes... */
+        {'-', block_size / 2},
+        /* Add data, converting pending zeroes to data and consume pending. */
+        {'\0', block_size / 2},
+        /* Add pending data... */
+        {'E', block_size / 2},
+        /* Add zeroes, converting zeroes to data and consume pending. */
+        {'-', block_size / 2},
+        /* Add pending zeroes... */
+        {'-', block_size / 2},
+        /* Add data, converting pending zeroes to data and consume pending. */
+        {'\0', block_size / 2},
+    };
+    char hexdigest[hexdigest_len];
+    checksum(extents, ARRAY_SIZE(extents), block_size, digest_name, hexdigest);
+    TEST_ASSERT_EQUAL_STRING("7a64eb9e5b568a4c912f9749aadd305fb463e614", hexdigest);
+}
+
+void test_mix_unaligned()
+{
+    struct extent extents[] = {
+        /* Add pending data... */
+        {'A', block_size / 2},
+        /* Add zeroes converting zeroes to data and consume pending. */
+        {'-', block_size / 2},
+        /* Add pending zeroes... */
+        {'-', block_size / 2},
+        /* Add data, converting pending zeroes to data and consume pending. */
+        {'\0', block_size / 2},
+        /* Add pending data... */
+        {'E', block_size / 2},
+        /* Add zeroes, converting zeroes to data and consume pending. */
+        {'-', block_size / 2},
+        /* Add pending zeroes... */
+        {'-', block_size / 2},
+        /* Consume pending zeroes. */
+    };
+    char hexdigest[hexdigest_len];
+    checksum(extents, ARRAY_SIZE(extents), block_size, digest_name, hexdigest);
+    TEST_ASSERT_EQUAL_STRING("dbaf0d2128c147d032842474c04918cc57f8da68", hexdigest);
+}
+
 int main(void)
 {
     UNITY_BEGIN();
 
     RUN_TEST(test_block_data);
     RUN_TEST(test_block_data_zero);
+    RUN_TEST(test_block_zero);
 
     RUN_TEST(test_partial_block_data);
     RUN_TEST(test_partial_block_data_zero);
+    RUN_TEST(test_partial_block_zero);
+
+    RUN_TEST(test_sparse);
+    RUN_TEST(test_sparse_unaligned);
+    RUN_TEST(test_sparse_large);
 
     RUN_TEST(test_zero);
     RUN_TEST(test_zero_unaligned);
 
     RUN_TEST(test_full);
     RUN_TEST(test_full_unaligned);
+
+    RUN_TEST(test_mix);
+    RUN_TEST(test_mix_unaligned);
 
     return UNITY_END();
 }
