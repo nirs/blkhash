d78dfc9b82873973193229d7ed2117b72a3a8724
Nir Soffer <nsoffer@redhat.com>
2021-04-07 03:11:47 +0300

    nbd: Support extents
    Source have now can_extent flag telling if the source support getting
    extents. The nbd source is the only source implementing extents.
    
    Maybe later we will add extents support for the file source.

--- nbd.c
+++ nbd.c
@@ -20,6 +20,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <libnbd.h>
 
@@ -28,6 +29,12 @@
 struct nbd_src {
     struct src src;
     struct nbd_handle *h;
+};
+
+struct extent_request {
+    int64_t length;
+    struct extent *extents;
+    size_t count;
 };
 
 static ssize_t nbd_ops_pread(struct src *s, void *buf, size_t len, int64_t offset)
@@ -49,6 +56,80 @@ static ssize_t nbd_ops_pread(struct src *s, void *buf, size_t len, int64_t offse
     return len;
 }
 
+static int extent_callback (void *user_data, const char *metacontext,
+                            uint64_t offset, uint32_t *entries,
+                            size_t nr_entries, int *error)
+{
+    struct extent_request *r = user_data;
+    size_t count = nr_entries / 2;
+    int64_t reply_length = 0;
+    size_t i;
+
+    if (strcmp(metacontext, LIBNBD_CONTEXT_BASE_ALLOCATION) != 0) {
+        DEBUG("unexpected meta context: %s", metacontext);
+        return 1;
+    }
+
+    r->extents = malloc(count * sizeof(*r->extents));
+    if (r->extents == NULL) {
+        perror("malloc");
+        exit(1);
+    }
+
+    /*
+     * NBD protocol allows the last request to end after the specified
+     * range. This is bad for our use case so we clip the last extent to
+     * the specified range.
+     *
+     * To be more robust against incompliant servers stop iteration once we
+     * reach the requested range, so we may report less extents.
+     */
+
+    for (i = 0; i < count && reply_length < r->length; i++) {
+        uint32_t length = entries[i * 2];
+        uint32_t flags = entries[i * 2 + 1];
+
+        if (reply_length + length > r->length) {
+            length = r->length - reply_length;
+        }
+
+        reply_length += length;
+        r->extents[i].length = length;
+        r->extents[i].zero = flags & LIBNBD_STATE_ZERO;
+    }
+
+    r->count = i;
+
+    return 1;
+}
+
+static int nbd_ops_extents(struct src *s, int64_t offset, int64_t length,
+                           struct extent **extents, size_t *count)
+{
+    struct nbd_src *ns = (struct nbd_src *)s;
+    struct extent_request r = { .length=length };
+    nbd_extent_callback cb = { .callback=extent_callback, .user_data=&r, };
+
+    if (!ns->src.can_extents)
+        return -1;
+
+    if (nbd_block_status(ns->h, length, offset, cb, 0)) {
+        /*
+         * Extents are a performance optimization, we can compute
+         * checksum without extents, slower.
+         */
+        DEBUG("%s", nbd_get_error());
+        ns->src.can_extents = false;
+        return -1;
+    }
+
+    /* Caller need to free extents. */
+    *extents = r.extents;
+    *count = r.count;
+
+    return 0;
+}
+
 static void nbd_ops_close(struct src *s)
 {
     struct nbd_src *ns = (struct nbd_src *)s;
@@ -61,6 +142,7 @@ static void nbd_ops_close(struct src *s)
 
 static struct src_ops nbd_ops = {
     .pread = nbd_ops_pread,
+    .extents = nbd_ops_extents,
     .close = nbd_ops_close,
 };
 
@@ -72,6 +154,11 @@ struct src *open_nbd(const char *uri)
     h = nbd_create();
     if (h == NULL) {
         perror("nbd_create");
+        exit(1);
+    }
+
+    if (nbd_add_meta_context(h, LIBNBD_CONTEXT_BASE_ALLOCATION)) {
+        fprintf (stderr, "%s\n", nbd_get_error());
         exit(1);
     }
 
@@ -88,6 +175,8 @@ struct src *open_nbd(const char *uri)
 
     ns->src.ops = &nbd_ops;
     ns->src.size = nbd_get_size(h);
+    ns->src.can_extents = nbd_can_meta_context(
+        h, LIBNBD_CONTEXT_BASE_ALLOCATION) > 0;
     ns->h = h;
 
     return &ns->src;
