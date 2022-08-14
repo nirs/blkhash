e44e9e04f31f7e915d16d6bdbf1bcbcc335303c0
Nir Soffer <nsoffer@redhat.com>
2022-08-14 14:20:20 +0300

    Add zero benchmark
    Example run on Lenovo ThinkPad P1 Gen 3:
    
        $ build/zero_bench
        aligned data best: 14000.00 GiB in 0.96 seconds (14595.98 GiB/s)
        aligned data worst: 60.00 GiB in 0.96 seconds (62.59 GiB/s)
        aligned zero: 60.00 GiB in 0.98 seconds (61.37 GiB/s)
        unaligned data best: 14000.00 GiB in 0.98 seconds (14337.74 GiB/s)
        unaligned data worst: 60.00 GiB in 0.96 seconds (62.23 GiB/s)
        unaligned zero: 60.00 GiB in 0.97 seconds (61.63 GiB/s)
    
    Example run no Dell PowerEdge R640:
    
        $ build/zero_bench
        aligned data best: 14000.00 GiB in 1.66 seconds (8427.26 GiB/s)
        aligned data worst: 60.00 GiB in 1.20 seconds (49.83 GiB/s)
        aligned zero: 60.00 GiB in 1.20 seconds (50.19 GiB/s)
        unaligned data best: 14000.00 GiB in 1.62 seconds (8667.36 GiB/s)
        unaligned data worst: 60.00 GiB in 1.20 seconds (49.88 GiB/s)
        unaligned zero: 60.00 GiB in 1.18 seconds (50.64 GiB/s)

---
+++ zero_bench.c
@@ -0,0 +1,107 @@
+// SPDX-FileCopyrightText: Red Hat Inc
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "blkhash_internal.h"
+#include "util.h"
+
+#define BLOCK_SIZE (64*1024L)
+
+#define MiB (1L<<20)
+#define GiB (1L<<30)
+#define TiB (1L<<40)
+
+static unsigned char *buffer;
+static bool quick;
+
+void bench(const char *name, uint64_t size, const void *buf, bool zero)
+{
+    int64_t start, elapsed;
+    double seconds;
+    double rate;
+
+    if (quick)
+        size /= 100;
+
+    start = gettime();
+
+    for (uint64_t i = 0; i < size / BLOCK_SIZE; i++) {
+        bool result = is_zero(buf, BLOCK_SIZE);
+        assert(result == zero);
+    }
+
+    elapsed = gettime() - start;
+    seconds = elapsed / 1e6;
+    rate = size / seconds / GiB;
+
+    printf("%s: %.2f GiB in %.2f seconds (%.2f GiB/s)\n",
+           name, (double)size / GiB, seconds, rate);
+}
+
+void bench_aligned_data_best()
+{
+    assert(((uintptr_t)buffer % 8) == 0);
+    memset(buffer, 0, BLOCK_SIZE);
+    buffer[7] = 0x55;
+    bench("aligned data best", 14000 * GiB, buffer, false);
+}
+
+void bench_aligned_data_worst()
+{
+    memset(buffer, 0, BLOCK_SIZE);
+    buffer[BLOCK_SIZE-1] = 0x55;
+    bench("aligned data worst", 60 * GiB, buffer, false);
+}
+
+void bench_aligned_zero()
+{
+    memset(buffer, 0, BLOCK_SIZE);
+    bench("aligned zero", 60 * GiB, buffer, true);
+}
+
+void bench_unaligned_data_best()
+{
+    unsigned char *p = buffer + 5;
+    assert(((uintptr_t)p % 8) != 0);
+    memset(p, 0, BLOCK_SIZE);
+    p[7] = 0x55;
+    bench("unaligned data best", 14000 * GiB, p, false);
+}
+
+void bench_unaligned_data_worst()
+{
+    unsigned char *p = buffer + 5;
+    memset(p, 0, BLOCK_SIZE);
+    p[BLOCK_SIZE-1] = 0x55;
+    bench("unaligned data worst", 60 * GiB, p, false);
+}
+
+void bench_unaligned_zero()
+{
+    unsigned char *p = buffer + 5;
+    memset(p, 0, BLOCK_SIZE);
+    bench("unaligned zero", 60 * GiB, p, true);
+}
+
+int main(int argc, char *argv[])
+{
+    /* Extract bytes for testing unalinged buffers. */
+    buffer = malloc(BLOCK_SIZE + 5);
+    assert(buffer);
+
+    /* Minimal test for CI and build machines. */
+    quick = (argc > 1 && strcmp(argv[1], "quick") == 0);
+
+    bench_aligned_data_best();
+    bench_aligned_data_worst();
+    bench_aligned_zero();
+
+    bench_unaligned_data_best();
+    bench_unaligned_data_worst();
+    bench_unaligned_zero();
+
+    free(buffer);
+}
