57f6b48de4439e8f43cb74195a3a47c73b5db4c2
Nir Soffer <nsoffer@redhat.com>
2022-08-27 20:29:05 +0300

    Handle errors in the example program
    Example code is typically missing error handling to make the code
    simpler. Since people copy and paste example code, this typically ends
    in broken user programs.
    
    Change the example program to handle errors properly:
    - Using errno to get errors from `blkhash_new()`.
    - using the return value for all other blkash calls.
    - Ensure that `blkash_free()` is always called.
    
    Part of #57

--- example.c
+++ example.c
@@ -3,6 +3,8 @@
 
 /* blkhash example program. */
 
+#include <errno.h>
+#include <stdio.h>
 #include <string.h>
 #include <openssl/evp.h>
 #include <blkhash.h>
@@ -10,42 +12,65 @@
 #define BLOCK_SIZE (64 * 1024)
 #define BUF_SIZE (2 * 1024 * 1024)
 
-int main(int argc, char *argv[])
+int main()
 {
-    struct blkhash *h;
-    unsigned char *buf;
+    struct blkhash *h = NULL;
+    unsigned char *buf = NULL;
     unsigned char md[EVP_MAX_MD_SIZE];
     unsigned int md_len, i;
+    int err;
 
     h = blkhash_new(BLOCK_SIZE, "sha1");
+    if (h == NULL) {
+        err = errno;
+        goto out;
+    }
 
     buf = malloc(BUF_SIZE);
+    if (buf == NULL) {
+        err = errno;
+        goto out;
+    }
 
     /* Hash some data. */
 
     memset(buf, 'A', BUF_SIZE);
-    blkhash_update(h, buf, BUF_SIZE);
+    err = blkhash_update(h, buf, BUF_SIZE);
+    if (err)
+        goto out;
 
     /* Hash more data, detecting zero blocks. */
 
     memset(buf, '\0', BUF_SIZE);
-    blkhash_update(h, buf, BUF_SIZE);
+    err = blkhash_update(h, buf, BUF_SIZE);
+    if (err)
+        goto out;
 
     /* Hash 1G of zeros. */
 
-    blkhash_zero(h, 1024 * 1024 * 1024);
+    err = blkhash_zero(h, 1024 * 1024 * 1024);
+    if (err)
+        goto out;
 
     /* Finalize the hash and print a message digest. */
 
-    blkhash_final(h, md, &md_len);
+    err = blkhash_final(h, md, &md_len);
+    if (err)
+        goto out;
 
     for (i = 0; i < md_len; i++)
         printf("%02x", md[i]);
 
     printf("\n");
 
+out:
     blkhash_free(h);
     free(buf);
+
+    if (err) {
+        fprintf(stderr, "Example failed: %s\n", strerror(err));
+        exit(EXIT_FAILURE);
+    }
 
     return 0;
 }
