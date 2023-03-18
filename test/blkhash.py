4334ae32924813317967875098829b1d3b7aa635
Nir Soffer <nsoffer@redhat.com>
2023-03-18 02:34:41 +0200

    Prototype for configurable number of threads
    Make the number of threads configurable by using constant number of
    streams and variable number of workers.
    
    When a block is mapped to stream, we also map the stream to one of the
    workers. This worker compute the block digest and add it to the stream.
    
    The number of streams is still 4 to keep the same hash before this
    change. The number of workers can change now from 1 to 4.
    
    We will choose a better default streams value later after implementing
    in blkhash, and testing scalability on big machines.
    
    Example run showing that we keep the same hash, and the number of
    workers do not affect the hash:
    
        $ blksum empty-1g.raw
        9b3d2f329b8e1a3a10ac623efa163c12e953dbb5192825b4772dcf0f8905e1b1  empty-1g.raw
    
        $ test/blkhash.py -t1 empty-1g.raw
        9b3d2f329b8e1a3a10ac623efa163c12e953dbb5192825b4772dcf0f8905e1b1  empty-1g.raw
    
        $ test/blkhash.py -t2 empty-1g.raw
        9b3d2f329b8e1a3a10ac623efa163c12e953dbb5192825b4772dcf0f8905e1b1  empty-1g.raw
    
        $ test/blkhash.py -t4 empty-1g.raw
        9b3d2f329b8e1a3a10ac623efa163c12e953dbb5192825b4772dcf0f8905e1b1  empty-1g.raw
    
    Testing show that this scale nicely up to 4 threads:
    
        $ hyperfine -w1 -r5 -p "sleep 10" -L t 1,2,4 "test/blkhash.py -t {t} full-6g.raw"
        Benchmark 1: test/blkhash.py -t 1 full-6g.raw
          Time (mean ± σ):     11.853 s ±  0.042 s    [User: 12.243 s, System: 0.741 s]
          Range (min … max):   11.786 s … 11.895 s    5 runs
    
        Benchmark 2: test/blkhash.py -t 2 full-6g.raw
          Time (mean ± σ):      5.965 s ±  0.087 s    [User: 12.130 s, System: 0.706 s]
          Range (min … max):    5.873 s …  6.105 s    5 runs
    
        Benchmark 3: test/blkhash.py -t 4 full-6g.raw
          Time (mean ± σ):      3.215 s ±  0.027 s    [User: 12.812 s, System: 0.698 s]
          Range (min … max):    3.177 s …  3.242 s    5 runs
    
        Summary
          'test/blkhash.py -t 4 full-6g.raw' ran
            1.86 ± 0.03 times faster than 'test/blkhash.py -t 2 full-6g.raw'
            3.69 ± 0.03 times faster than 'test/blkhash.py -t 1 full-6g.raw'
    
    Testing shows that number of streams does not affects the performance:
    
        $ hyperfine -w1 -r3 -p "sleep 10" -L s 64,32,16,8,4 "test/blkhash.py -s {s} empty-6g.raw"
        Benchmark 1: test/blkhash.py -s 64 empty-6g.raw
          Time (mean ± σ):      3.200 s ±  0.053 s    [User: 12.628 s, System: 0.782 s]
          Range (min … max):    3.170 s …  3.262 s    3 runs
    
        Benchmark 2: test/blkhash.py -s 32 empty-6g.raw
          Time (mean ± σ):      3.203 s ±  0.052 s    [User: 12.571 s, System: 0.810 s]
          Range (min … max):    3.165 s …  3.262 s    3 runs
    
        Benchmark 3: test/blkhash.py -s 16 empty-6g.raw
          Time (mean ± σ):      3.165 s ±  0.040 s    [User: 12.562 s, System: 0.735 s]
          Range (min … max):    3.132 s …  3.210 s    3 runs
    
        Benchmark 4: test/blkhash.py -s 8 empty-6g.raw
          Time (mean ± σ):      3.221 s ±  0.014 s    [User: 12.697 s, System: 0.755 s]
          Range (min … max):    3.209 s …  3.236 s    3 runs
    
        Benchmark 5: test/blkhash.py -s 4 empty-6g.raw
          Time (mean ± σ):      3.203 s ±  0.025 s    [User: 12.647 s, System: 0.766 s]
          Range (min … max):    3.174 s …  3.223 s    3 runs
    
        Summary
          'test/blkhash.py -s 16 empty-6g.raw' ran
            1.01 ± 0.02 times faster than 'test/blkhash.py -s 64 empty-6g.raw'
            1.01 ± 0.02 times faster than 'test/blkhash.py -s 32 empty-6g.raw'
            1.01 ± 0.02 times faster than 'test/blkhash.py -s 4 empty-6g.raw'
            1.02 ± 0.01 times faster than 'test/blkhash.py -s 8 empty-6g.raw'
    
    Part-of: #79

--- test/blkhash.py
+++ test/blkhash.py
@@ -17,7 +17,10 @@ Usage:
 
 import argparse
 import hashlib
+import queue
+import threading
 
+from collections import namedtuple
 from functools import partial
 
 # Values must match the compiled valeus in blksum.c.
@@ -25,26 +28,61 @@ from functools import partial
 BLOCK_SIZE = 64 * 1024
 STREAMS = 4
 
+# Values that do not affect te hash value.
+THREADS = 4
+QUEUE_SIZE = 16
+READ_SIZE = 1024**2
+
 
 def main():
     p = argparse.ArgumentParser()
     p.add_argument(
-        "-d", "--digest-name",
+        "-d",
+        "--digest-name",
         default="sha256",
-        help="Digest name (default sha256)")
+        help="Digest name (default sha256)",
+    )
     p.add_argument(
-        "filename",
-        help="Filename to checksum")
+        "-t",
+        "--threads",
+        type=int,
+        default=THREADS,
+        help=f"Number of threads (default {THREADS})",
+    )
+    p.add_argument(
+        "-s",
+        "--streams",
+        type=int,
+        default=STREAMS,
+        help=f"Number of streams (default {STREAMS})",
+    )
+    p.add_argument("filename", help="Filename to checksum")
 
     args = p.parse_args()
-    blkhash = checksum(args.filename, args.digest_name)
+    blkhash = checksum(
+        args.filename,
+        args.digest_name,
+        threads=args.threads,
+        streams=args.streams,
+    )
     print(f"{blkhash}  {args.filename}")
 
 
-def checksum(filename, digest_name, block_size=BLOCK_SIZE, read_size=1024**2):
+def checksum(
+    filename,
+    digest_name,
+    block_size=BLOCK_SIZE,
+    threads=THREADS,
+    streams=STREAMS,
+):
     with open(filename, "rb") as f:
-        h = Blkhash(digest_name, block_size=block_size)
-        for data in iter(partial(f.read, read_size), b""):
+        h = Blkhash(
+            digest_name,
+            block_size=block_size,
+            threads=threads,
+            streams=streams,
+        )
+        for data in iter(partial(f.read, READ_SIZE), b""):
             h.update(data)
         return h.hexdigest()
 
@@ -68,12 +106,38 @@ class Blkhash:
         H(stream 2) = H( H(block 2) || H(block 6) || H(block 10) || H(block 14) )
         H(stream 3) = H( H(block 3) || H(block 7) || H(block 11) || H(block 15) )
 
+    We use T threads, each stream is mapped to thread matching:
+
+        thread_index = stream_index % T
+
+    Example with 2 threads:
+
+        thread 0: stream 0, stream 2
+        thread 1: stream 1, stream 3
+
+    Example with 4 threads:
+
+        thread 0: stream 0
+        thread 1: stream 1
+        thread 2: stream 2
+        thread 3: stream 3
+
     """
 
-    def __init__(self, digest_name, block_size=BLOCK_SIZE, streams=STREAMS):
+    def __init__(
+        self,
+        digest_name,
+        block_size=BLOCK_SIZE,
+        threads=THREADS,
+        streams=STREAMS,
+    ):
+        assert threads > 0
+        assert threads <= streams
+
         self.digest_name = digest_name
         self.block_size = block_size
-        self.streams = [hashlib.new(digest_name) for i in range(streams)]
+        self.streams = [hashlib.new(digest_name) for _ in range(streams)]
+        self.workers = [Worker(digest_name) for _ in range(threads)]
         self.pending = None
         self.index = 0
         self.finalized = False
@@ -92,16 +156,24 @@ class Blkhash:
                     self.pending = None
 
             while len(view) >= self.block_size:
-                self._add_block(view[:self.block_size])
-                view = view[self.block_size:]
+                self._add_block(view[: self.block_size])
+                view = view[self.block_size :]
 
             if len(view):
                 self.pending = bytearray(view)
 
+    def hexdigest(self):
+        self._finalize()
+        root = hashlib.new(self.digest_name)
+        for stream in self.streams:
+            root.update(stream.digest())
+        return root.hexdigest()
+
     def _add_block(self, data):
-        block_digest = hashlib.new(self.digest_name, data).digest()
-        stream = self.streams[self.index % len(self.streams)]
-        stream.update(block_digest)
+        stream_index = self.index % len(self.streams)
+        worker_index = stream_index % len(self.workers)
+        work = Work(data, self.streams[stream_index])
+        self.workers[worker_index].submit(work)
         self.index += 1
 
     def _finalize(self):
@@ -110,13 +182,40 @@ class Blkhash:
             if self.pending:
                 self._add_block(self.pending)
                 self.pending = None
+            for worker in self.workers:
+                worker.stop()
+            for worker in self.workers:
+                worker.wait()
 
-    def hexdigest(self):
-        self._finalize()
-        root = hashlib.new(self.digest_name)
-        for stream in self.streams:
-            root.update(stream.digest())
-        return root.hexdigest()
+
+Work = namedtuple("Work", "data, stream")
+
+
+class Worker:
+    def __init__(self, digest_name):
+        self.digest_name = digest_name
+        self.queue = queue.Queue(maxsize=QUEUE_SIZE)
+        self.thread = threading.Thread(target=self.run)
+        self.thread.daemon = True
+        self.thread.start()
+
+    def submit(self, work):
+        self.queue.put(work)
+
+    def stop(self):
+        self.queue.put(None)
+
+    def wait(self):
+        self.thread.join()
+
+    def run(self):
+        while True:
+            work = self.queue.get()
+            if work is None:
+                break
+
+            digest = hashlib.new(self.digest_name, work.data).digest()
+            work.stream.update(digest)
 
 
 if __name__ == "__main__":
