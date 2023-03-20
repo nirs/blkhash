84e3b2754dbe983351b4408cb2f1434577777b89
Nir Soffer <nsoffer@redhat.com>
2023-03-20 21:34:56 +0200

    Tests also 64 threads
    On big servers using 64 threads is practical, but not very useful.
    
    On my laptop with 12 cores, 64 threads are little slower than 32
    threads:
    
    blkhash-bench --digest-name sha256 --input-type data
    
     1 threads: 614.40 MiB in 1.096 s (560.45 MiB/s)
     2 threads: 1.00 GiB in 0.937 s (1.07 GiB/s)
     4 threads: 2.00 GiB in 0.944 s (2.12 GiB/s)
     8 threads: 2.20 GiB in 1.000 s (2.20 GiB/s)
    16 threads: 3.00 GiB in 1.001 s (3.00 GiB/s)
    32 threads: 2.90 GiB in 0.974 s (2.98 GiB/s)
    64 threads: 2.90 GiB in 1.031 s (2.81 GiB/s)
    
    blkhash-bench --digest-name sha256 --input-type hole
    
     1 threads: 960.00 GiB in 1.033 s (929.55 GiB/s)
     2 threads: 1.80 TiB in 1.043 s (1.73 TiB/s)
     4 threads: 3.50 TiB in 1.076 s (3.25 TiB/s)
     8 threads: 4.00 TiB in 1.139 s (3.51 TiB/s)
    16 threads: 4.70 TiB in 1.065 s (4.41 TiB/s)
    32 threads: 4.60 TiB in 1.048 s (4.39 TiB/s)
    64 threads: 4.60 TiB in 1.065 s (4.32 TiB/s)
    
    On machine with 80 cores 64 threads are 1.07 times faster than 32
    threads for data, but 1.9 times faster for holes.
    
    blkhash-bench --digest-name sha256 --input-type data
    
     1 threads: 614.40 MiB in 1.345 s (456.68 MiB/s)
     2 threads: 1.00 GiB in 1.140 s (897.99 MiB/s)
     4 threads: 2.00 GiB in 1.165 s (1.72 GiB/s)
     8 threads: 2.20 GiB in 0.680 s (3.24 GiB/s)
    16 threads: 3.00 GiB in 0.605 s (4.96 GiB/s)
    32 threads: 2.90 GiB in 0.490 s (5.92 GiB/s)
    64 threads: 2.90 GiB in 0.457 s (6.35 GiB/s)
    
    blkhash-bench --digest-name sha256 --input-type hole
    
     1 threads: 960.00 GiB in 1.241 s (773.28 GiB/s)
     2 threads: 1.80 TiB in 1.239 s (1.45 TiB/s)
     4 threads: 3.50 TiB in 1.238 s (2.83 TiB/s)
     8 threads: 4.00 TiB in 0.748 s (5.35 TiB/s)
    16 threads: 4.70 TiB in 0.556 s (8.45 TiB/s)
    32 threads: 4.60 TiB in 0.606 s (7.59 TiB/s)
    64 threads: 4.60 TiB in 0.316 s (14.54 TiB/s)

--- test/blkhash-bench.py
+++ test/blkhash-bench.py
@@ -12,6 +12,7 @@ bench.blkhash("data", "2.0g", digest_name="sha256", threads=4)
 bench.blkhash("data", "2.2g", digest_name="sha256", threads=8)
 bench.blkhash("data", "3.0g", digest_name="sha256", threads=16)
 bench.blkhash("data", "2.9g", digest_name="sha256", threads=32)
+bench.blkhash("data", "2.9g", digest_name="sha256", threads=64)
 
 print("\nblkhash-bench --digest-name sha256 --input-type zero\n")
 
@@ -21,6 +22,7 @@ bench.blkhash("zero", "50g", digest_name="sha256", threads=4)
 bench.blkhash("zero", "50g", digest_name="sha256", threads=8)
 bench.blkhash("zero", "50g", digest_name="sha256", threads=16)
 bench.blkhash("zero", "50g", digest_name="sha256", threads=32)
+bench.blkhash("zero", "50g", digest_name="sha256", threads=64)
 
 print("\nblkhash-bench --digest-name sha256 --input-type hole\n")
 
@@ -30,6 +32,7 @@ bench.blkhash("hole", "3.5t", digest_name="sha256", threads=4)
 bench.blkhash("hole", "4.0t", digest_name="sha256", threads=8)
 bench.blkhash("hole", "4.7t", digest_name="sha256", threads=16)
 bench.blkhash("hole", "4.6t", digest_name="sha256", threads=32)
+bench.blkhash("hole", "4.6t", digest_name="sha256", threads=64)
 
 print("\nblkhash-bench --digest-name null --input-type data\n")
 
@@ -39,6 +42,7 @@ bench.blkhash("data", "21g", digest_name="null", threads=4)
 bench.blkhash("data", "21g", digest_name="null", threads=8)
 bench.blkhash("data", "21g", digest_name="null", threads=16)
 bench.blkhash("data", "21g", digest_name="null", threads=32)
+bench.blkhash("data", "21g", digest_name="null", threads=64)
 
 print("\nblkhash-bench --digest-name null --input-type zero\n")
 
@@ -48,6 +52,7 @@ bench.blkhash("zero", "50g", digest_name="null", threads=4)
 bench.blkhash("zero", "50g", digest_name="null", threads=8)
 bench.blkhash("zero", "50g", digest_name="null", threads=16)
 bench.blkhash("zero", "50g", digest_name="null", threads=32)
+bench.blkhash("zero", "50g", digest_name="null", threads=64)
 
 print("\nblkhash-bench --digest-name null --input-type hole\n")
 
@@ -57,3 +62,4 @@ bench.blkhash("hole", "118t", digest_name="null", threads=4)
 bench.blkhash("hole", "157t", digest_name="null", threads=8)
 bench.blkhash("hole", "211t", digest_name="null", threads=16)
 bench.blkhash("hole", "190t", digest_name="null", threads=32)
+bench.blkhash("hole", "190t", digest_name="null", threads=64)
