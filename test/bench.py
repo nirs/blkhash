57aac7d084a9f7e83519b1808a09c433d5a553eb
Nir Soffer <nsoffer@redhat.com>
2023-03-20 22:35:12 +0200

    Simplify benchmark helpers
    There is no reason to have None default in the helper. Using real values
    makes the code simpler.

--- test/bench.py
+++ test/bench.py
@@ -21,16 +21,14 @@ GiB = 1 << 30
 TiB = 1 << 40
 
 
-def blkhash(input_type, input_size, digest_name=None, threads=None):
+def blkhash(input_type, input_size, digest_name="sha256", threads=4):
     cmd = [
         BLKHASH_BENCH,
         f"--input-type={input_type}",
         f"--input-size={input_size}",
+        f"--digest-name={digest_name}",
+        f"--threads={threads}",
     ]
-    if digest_name:
-        cmd.append(f"--digest-name={digest_name}")
-    if threads:
-        cmd.append(f"--threads={threads}")
     cp = subprocess.run(cmd, check=True, stdout=subprocess.PIPE)
     r = json.loads(cp.stdout)
     hsize = format_humansize(r["input-size"])
@@ -39,15 +37,17 @@ def blkhash(input_type, input_size, digest_name=None, threads=None):
     return r
 
 
-def openssl(input_size, digest_name=None):
-    cmd = [OPENSSL_BENCH, f"--input-size={input_size}"]
-    if digest_name:
-        cmd.append(f"--digest-name={digest_name}")
+def openssl(input_size, digest_name="sha256"):
+    cmd = [
+        OPENSSL_BENCH,
+        f"--input-size={input_size}",
+        f"--digest-name={digest_name}",
+    ]
     cp = subprocess.run(cmd, check=True, stdout=subprocess.PIPE)
     r = json.loads(cp.stdout)
     hsize = format_humansize(r["input-size"])
     hrate = format_humansize(r["throughput"])
-    print(f"{r['threads']:>2} threads: {hsize} in {r['elapsed']:.3f} s ({hrate}/s)")
+    print(f"{hsize} in {r['elapsed']:.3f} s ({hrate}/s)")
     return r
 
 
