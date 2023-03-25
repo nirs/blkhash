a6893cdfc3577525f3d6e037bddc57e81321df7c
Nir Soffer <nsoffer@redhat.com>
2023-03-25 13:17:31 +0300

    Optimize zero detection for data
    Since we know that we always have more than 16 bytes, we can optimize by
    checking the first 16 bytes as 2 64bit integers.
    
    Some platforms may require aligned memory, we copy the first 16 bytes to
    a temporary buffer before checking them.
    
    Testing show 12-50% improvement for detecting that data is not a zero
    block, but no measurable difference when computing a checksum. However
    the code is simpler.
    
    Example improvement on Macbook Air M1:
    
    Before:
    
        % build/test/zero-bench
        aligned data best: 27.00 TiB in 0.889 seconds (30.36 TiB/s)
        aligned data worst: 60.00 GiB in 1.662 seconds (36.09 GiB/s)
        aligned zero: 60.00 GiB in 1.644 seconds (36.51 GiB/s)
        unaligned data best: 27.00 TiB in 0.850 seconds (31.77 TiB/s)
        unaligned data worst: 60.00 GiB in 1.677 seconds (35.78 GiB/s)
        unaligned zero: 60.00 GiB in 1.654 seconds (36.28 GiB/s)
    
    After:
    
        % build/test/zero-bench
        aligned data best: 40.00 TiB in 0.878 seconds (45.54 TiB/s)
        aligned data worst: 60.00 GiB in 1.659 seconds (36.17 GiB/s)
        aligned zero: 60.00 GiB in 1.637 seconds (36.65 GiB/s)
        unaligned data best: 40.00 TiB in 0.839 seconds (47.69 TiB/s)
        unaligned data worst: 60.00 GiB in 1.666 seconds (36.02 GiB/s)
        unaligned zero: 60.00 GiB in 1.651 seconds (36.34 GiB/s)

--- lib/zero.c
+++ lib/zero.c
@@ -12,20 +12,19 @@
  */
 bool is_zero(const void *buf, size_t len)
 {
-    const unsigned char *p;
-    const unsigned char *c;
+    uint64_t n[2];
 
     /* We always use full blocks. */
     assert(len >= 16);
 
-    p = buf;
+    /* Ensure aligned memory, may be required by some architectures:
+     * https://www.kernel.org/doc/html/next/core-api/unaligned-memory-access.html */
+    memcpy(n, buf, 16);
 
     /* Check first 16 bytes manually. */
-    for (c = p; c < p + 16; c += 8) {
-        if (c[0] | c[1] | c[2] | c[4] | c[5] | c[6] | c[7])
-            return false;
-    }
+    if (n[0] | n[1])
+        return false;
 
     /* Now we know that's zero, memcmp with self. */
-    return memcmp(buf, p + 16, len - 16) == 0;
+    return memcmp(buf, buf + 16, len - 16) == 0;
 }
