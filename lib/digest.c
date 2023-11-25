06c6cb93d83af6dbf6f6e51f9eebb1d773be9199
Nir Soffer <nirsof@gmail.com>
2023-11-25 23:14:51 +0200

    Add openssl backend
    If the digest name is a builtin digest (currently only "null") fall back
    to openssl provided digests.
    
    Listing digests from openssl is not implemented yet.

--- lib/digest.c
+++ lib/digest.c
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <openssl/evp.h>
+
 #include "digest.h"
 
 static int null_init(struct digest *d)
@@ -52,6 +54,99 @@ static int create_null(struct digest **out)
     return 0;
 }
 
+struct evp_digest {
+    struct digest digest;
+#if OPENSSL_VERSION_NUMBER >= 0x30000000L
+    EVP_MD *md;
+#else
+    const EVP_MD *md;
+#endif
+    EVP_MD_CTX *ctx;
+};
+
+static int evp_init(struct digest *d)
+{
+    struct evp_digest *evp = (struct evp_digest *)d;
+
+    if (!EVP_DigestInit_ex(evp->ctx, evp->md, NULL))
+        return -ENOMEM;
+
+    return 0;
+}
+
+static int evp_update(struct digest *d, const void *data, size_t len)
+{
+    struct evp_digest *evp = (struct evp_digest *)d;
+
+    if (!EVP_DigestUpdate(evp->ctx, data, len))
+        return -ENOMEM;
+
+    return 0;
+}
+
+static int evp_final(struct digest *d, unsigned char *md, unsigned int *len)
+{
+    struct evp_digest *evp = (struct evp_digest *)d;
+
+    if (!EVP_DigestFinal_ex(evp->ctx, md, len))
+        return -ENOMEM;
+
+    return 0;
+}
+
+static void evp_destroy(struct digest *d)
+{
+    struct evp_digest *evp = (struct evp_digest *)d;
+
+#if OPENSSL_VERSION_NUMBER >= 0x30000000L
+    EVP_MD_free(evp->md);
+#endif
+    EVP_MD_CTX_free(evp->ctx);
+    free(evp);
+}
+
+struct digest_ops evp_ops = {
+    .init = evp_init,
+    .update = evp_update,
+    .finalize = evp_final,
+    .destroy = evp_destroy,
+};
+
+static int create_evp(const char *name, struct digest **out)
+{
+    struct evp_digest *evp;
+    int err = 0;
+
+    evp = calloc(1, sizeof(*evp));
+    if (evp == NULL)
+        return -errno;
+
+    evp->digest.ops = &evp_ops;
+
+#if OPENSSL_VERSION_NUMBER >= 0x30000000L
+    evp->md = EVP_MD_fetch(NULL, name, NULL);
+#else
+    evp->md = EVP_get_digestbyname(name);
+#endif
+    if (evp->md == NULL) {
+        err = -EINVAL;
+        goto error;
+    }
+
+    evp->ctx = EVP_MD_CTX_new();
+    if (evp->ctx == NULL) {
+        err = -ENOMEM;
+        goto  error;
+    }
+
+    *out = &evp->digest;
+    return 0;
+
+error:
+    evp_destroy(&evp->digest);
+    return err;
+}
+
 int digest_list(const char **names, size_t *len)
 {
     *names = calloc(1, sizeof(**names));
@@ -69,6 +164,5 @@ int digest_create(const char *name, struct digest **out)
     if (strcmp(name, "null") == 0)
         return create_null(out);
 
-    *out = NULL;
-    return -EINVAL;
+    return create_evp(name, out);
 }
--- test/digest-test.c
+++ test/digest-test.c
@@ -144,6 +144,98 @@ out:
     TEST_ASSERT_EQUAL_INT(0, err);
 }
 
+void test_sha256()
+{
+    struct test_vector tests[] = {
+        {
+            .name="empty string",
+            .message="",
+            .count=1,
+            .output="e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+        },
+        {
+            .name="abc",
+            .message="abc",
+            .count=1,
+            .output="ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad",
+        },
+        {
+            .name="abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq",
+            .message="abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq",
+            .count=1,
+            .output="248d6a61d20638b8e5c026930c3e6039a33ce45964ff2167f6ecedd419db06c1",
+        },
+        {
+            .name="abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu",
+            .message="abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu",
+            .count=1,
+            .output="cf5b16a778af8380036ce59e7b0492370b249b11e8f07a51afac45037afee9d1",
+        },
+        {
+            .name="'a' repeated 1,000,000 times",
+            .message="a",
+            .count=1000000,
+            .output="cdc76e5c9914fb9281a1c7e284d73e67f1809a48a497200e046d39ccc7112cd0",
+        },
+        {
+            .name="'abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmno' repeated 16,777,216 times",
+            .message="a",
+            .count=16777216,
+            .output="5b6ff2e19d0da0fe323061018fc381393492884e74af8296c81ab9cb2694783a",
+        },
+    };
+
+    for (unsigned i = 0; i < ARRAY_SIZE(tests); i++) {
+        check("sha256", &tests[i]);
+    }
+}
+
+void test_blake2b512()
+{
+    struct test_vector tests[] = {
+        {
+            .name="empty string",
+            .message="",
+            .count=1,
+            .output="786a02f742015903c6c6fd852552d272912f4740e15847618a86e217f71f5419d25e1031afee585313896444934eb04b903a685b1448b755d56f701afe9be2ce",
+        },
+        {
+            .name="abc",
+            .message="abc",
+            .count=1,
+            .output="ba80a53f981c4d0d6a2797b69f12f6e94c212f14685ac4b74b12bb6fdbffa2d17d87c5392aab792dc252d5de4533cc9518d38aa8dbf1925ab92386edd4009923",
+        },
+        {
+            .name="abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq",
+            .message="abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq",
+            .count=1,
+            .output="7285ff3e8bd768d69be62b3bf18765a325917fa9744ac2f582a20850bc2b1141ed1b3e4528595acc90772bdf2d37dc8a47130b44f33a02e8730e5ad8e166e888",
+        },
+        {
+            .name="abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu",
+            .message="abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu",
+            .count=1,
+            .output="ce741ac5930fe346811175c5227bb7bfcd47f42612fae46c0809514f9e0e3a11ee1773287147cdeaeedff50709aa716341fe65240f4ad6777d6bfaf9726e5e52",
+        },
+        {
+            .name="'a' repeated 1,000,000 times",
+            .message="a",
+            .count=1000000,
+            .output="98fb3efb7206fd19ebf69b6f312cf7b64e3b94dbe1a17107913975a793f177e1d077609d7fba363cbba00d05f7aa4e4fa8715d6428104c0a75643b0ff3fd3eaf",
+        },
+        {
+            .name="'abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmno' repeated 16,777,216 times",
+            .message="a",
+            .count=16777216,
+            .output="096ce550e899a2b88cad350fa43175e24592a547cdace5e6330167bff92a2db427887d4221fae00ca681a323ebd18e6d67678c1f3ab6c2a00e09fbec601afdb9",
+        },
+    };
+
+    for (unsigned i = 0; i < ARRAY_SIZE(tests); i++) {
+        check("blake2b512", &tests[i]);
+    }
+}
+
 int main(void)
 {
     UNITY_BEGIN();
@@ -152,6 +244,8 @@ int main(void)
     RUN_TEST(test_unknown);
     RUN_TEST(test_null);
     RUN_TEST(test_null_no_size);
+    RUN_TEST(test_sha256);
+    RUN_TEST(test_blake2b512);
 
     return UNITY_END();
 }
