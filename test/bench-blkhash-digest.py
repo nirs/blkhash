6dc8f266b846e63cfa3d60ad13277db3eb7a4b20
Nir Soffer <nsoffer@redhat.com>
2023-12-09 00:58:54 +0200

    Add blkhash digest benchmark
    Compare blake3, blake2b512, sha512-256, sha256, sha3-256 using
    blkhash-bench and data input type.

---
+++ test/bench-blkhash-digest.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+# SPDX-FileCopyrightText: Red Hat Inc
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+import bench
+
+args = bench.parse_args()
+
+results = bench.results(
+    "blkhash - digests",
+    host_name=args.host_name,
+)
+
+print(f"\nblkhash-bench --digest-name blake3 --input-type data --aio\n")
+runs = []
+results["data"].append({"name": "blk-blake3", "runs": runs})
+for n in bench.threads(args.max_threads):
+    r = bench.blkhash(
+        "data",
+        digest_name="blake3",
+        threads=n,
+        aio=True,
+        queue_depth=32,
+        timeout_seconds=args.timeout,
+        cool_down=args.cool_down,
+    )
+    runs.append(r)
+
+print(f"\nblkhash-bench --digest-name blake2b512 --input-type data --aio\n")
+runs = []
+results["data"].append({"name": "blk-blake2b512", "runs": runs})
+for n in bench.threads(args.max_threads):
+    r = bench.blkhash(
+        "data",
+        digest_name="blake2b512",
+        threads=n,
+        aio=True,
+        queue_depth=32,
+        timeout_seconds=args.timeout,
+        cool_down=args.cool_down,
+    )
+    runs.append(r)
+
+print(f"\nblkhash-bench --digest-name sha512-256 --input-type data --aio\n")
+runs = []
+results["data"].append({"name": "blk-sha512-256", "runs": runs})
+for n in bench.threads(args.max_threads):
+    r = bench.blkhash(
+        "data",
+        digest_name="sha512-256",
+        threads=n,
+        aio=True,
+        queue_depth=32,
+        timeout_seconds=args.timeout,
+        cool_down=args.cool_down,
+    )
+    runs.append(r)
+
+print(f"\nblkhash-bench --digest-name sha256 --input-type data --aio\n")
+runs = []
+results["data"].append({"name": "blk-sha256", "runs": runs})
+for n in bench.threads(args.max_threads):
+    r = bench.blkhash(
+        "data",
+        digest_name="sha256",
+        threads=n,
+        aio=True,
+        queue_depth=32,
+        timeout_seconds=args.timeout,
+        cool_down=args.cool_down,
+    )
+    runs.append(r)
+
+print(f"\nblkhash-bench --digest-name sha3-256 --input-type data --aio\n")
+runs = []
+results["data"].append({"name": "blk-sha3-256", "runs": runs})
+for n in bench.threads(args.max_threads):
+    r = bench.blkhash(
+        "data",
+        digest_name="sha3-256",
+        threads=n,
+        aio=True,
+        queue_depth=32,
+        timeout_seconds=args.timeout,
+        cool_down=args.cool_down,
+    )
+    runs.append(r)
+
+if args.output:
+    bench.write(results, args.output)
