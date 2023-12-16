de9921e807bbc5eb0bf4c689ebca97c9ea10d14d
Nir Soffer <nsoffer@redhat.com>
2023-12-16 23:53:17 +0200

    Add benchmark scripts for mmap-bench

---
+++ test/bench-mmap-blake3.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+
+base=/data/tmp/blksum
+
+for image in 20p 40p 80p; do
+    for i in 1 2 3; do
+        dd if=$base/$image.raw bs=1M of=/dev/null
+    done
+    hyperfine --parameter-list t 1,2,3,4,5,6,7,8,9,10,11,12 \
+        --export-json "mmap-blake3-$image.json" \
+        --prepare "sleep 3" \
+        --runs 3 \
+        --time-unit second \
+        "build/test/mmap-bench -d blake3 -t {t} -a $base/$image.raw"
+done
