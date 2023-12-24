ac87bb40495e7758b53c393989323c491e433c25
Nir Soffer <nsoffer@redhat.com>
2023-12-24 02:54:45 +0200

    Implement flat merkle tree
    Replace the streams based construction with flat merkle tree:
    
        H( H(block 1) || H(block 2) || ... || H(block N) )
    
    To maintain block order, submissions are are kept in a submission FIFO
    queue managed by the blkhash thread.
    
    Zero blocks are queued in the submission queue, and added to the root
    when they reach the front of the queue.
    
    Data blocks are queued in the submission queue, and submitted to a
    thread pool for detecting zeros and computing the block hash.  When the
    operation is completed, the hash are added in order to the root hash.
    
    When the thread pool detects a zero blocks, it marks the submission as
    zero block and complete the submission. On the submission queue, the
    blkhash thread add a zero block hash to the root hash.
    
    If submitting to the thread pool failed, or computing a block hash
    failed, the submission is marked as failure. The blkhash thread will
    fail the entire operation when detecting a failed submission.
    
    Unfinished:
    - hash-pool queue likely has false sharing issues - test with perf C2C
      https://man7.org/linux/man-pages/man1/perf-c2c.1.html
    - submission_queue_{pop,push} interface is horrible, need to change to
      keep submission pointers, and allocated the submissions elsewhere. If
      malloc is a problem we can use a simple memory pool.
    - remove streams option from blkhash_opts
    - update manual and docs
    - update blkhash-test with new construction
    - replace test/blkhash.py with test/blkhash_flat.py

---
+++ lib/hash-pool.c
@@ -0,0 +1,271 @@
+// SPDX-FileCopyrightText: Red Hat Inc
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+#include <errno.h>
+
+#include "blkhash-internal.h"
+#include "digest.h"
+#include "hash-pool.h"
+#include "submission.h"
+#include "threads.h"
+
+static struct submission *STOP = (struct submission *)-1;
+
+static void set_error(struct hash_pool *p, int error)
+{
+    mutex_lock(&p->mutex);
+
+    /* Keep the first error. */
+    if (p->error == 0)
+        p->error = error;
+
+    mutex_unlock(&p->mutex);
+}
+
+static inline void push_unlocked(struct hash_pool *p, struct submission *sub)
+{
+    p->queue[p->queue_tail] = sub;
+    p->queue_tail = (p->queue_tail + 1) % p->queue_size;
+    p->queue_len++;
+}
+
+static inline struct submission *pop_unlocked(struct hash_pool *p)
+{
+    struct submission *sub;
+
+    sub = p->queue[p->queue_head];
+    p->queue_head = (p->queue_head + 1) % p->queue_size;
+    p->queue_len--;
+
+    return sub;
+}
+
+static struct submission *wait_for_work(struct hash_pool *p)
+{
+    struct submission *sub;
+
+    mutex_lock(&p->mutex);
+
+    while (p->queue_len == 0) {
+        p->workers_idle++;
+        cond_wait(&p->not_empty, &p->mutex);
+        p->workers_idle--;
+    }
+
+    sub = pop_unlocked(p);
+
+    mutex_unlock(&p->mutex);
+
+    return sub;
+}
+
+static bool is_zero_block(struct hash_pool *p, const struct submission *sub)
+{
+    return !(sub->flags & SUBMIT_COPY_DATA) &&
+        sub->len == p->config->block_size &&
+        is_zero(sub->data, sub->len);
+}
+
+static void compute_block_digest(struct submission *sub, struct digest *digest)
+{
+    int err;
+
+    err = -digest_init(digest);
+    if (err)
+        goto error;
+
+    err = -digest_update(digest, sub->data, sub->len);
+    if (err)
+        goto error;
+
+    err = -digest_final(digest, sub->md, NULL);
+    if (err)
+        goto error;
+
+    return;
+
+error:
+    submission_set_error(sub, err);
+}
+
+static void *worker_thread(void *arg)
+{
+    struct hash_pool *p = arg;
+    struct submission *sub;
+    struct digest *digest;
+    int err;
+
+    err = digest_create(p->config->digest_name, &digest);
+    if (err) {
+        set_error(p, err);
+        return NULL;
+    }
+
+    for (;;) {
+        sub = wait_for_work(p);
+        if (sub == STOP)
+            break;
+
+        if (is_zero_block(p, sub))
+            sub->type = ZERO;
+        else
+            compute_block_digest(sub, digest);
+
+        submission_complete(sub);
+    }
+
+    digest_destroy(digest);
+
+    return NULL;
+}
+
+static void stop_workers(struct hash_pool *p)
+{
+    mutex_lock(&p->mutex);
+
+    /* New sumissions will fail with EIO. */
+    p->stopped = true;
+
+    /* Clear the queue, failing all queued submissions. */
+    while (p->queue_len > 0) {
+        struct submission *sub = pop_unlocked(p);
+        submission_set_error(sub, EIO);
+        submission_complete(sub);
+    }
+
+    /* Add STOP submission for every worker. */
+    for (unsigned i = 0; i < p->workers_count; i++) {
+        push_unlocked(p, STOP);
+    }
+
+    /* Wake up all workers and wait until they terminate. */
+    cond_broadcast(&p->not_empty);
+
+    for (unsigned i = 0; i < p->workers_count; i++) {
+        mutex_unlock(&p->mutex);
+        pthread_join(p->workers[i], NULL);
+        mutex_lock(&p->mutex);
+    }
+
+    mutex_unlock(&p->mutex);
+}
+
+int hash_pool_init(struct hash_pool *p, const struct config *config)
+{
+    int err;
+
+    p->config = config;
+    p->queue_size = config->max_submissions;
+    p->queue_len = 0;
+    p->queue_head = 0;
+    p->queue_tail = 0;
+    p->workers_count = 0;
+    p->workers_idle = 0;
+    p->stopped = false;
+    p->error = 0;
+
+    p->queue = calloc(p->queue_size, sizeof(*p->queue));
+    if (p->queue == NULL)
+        return errno;
+
+    p->workers = calloc(config->workers, sizeof(*p->workers));
+    if (p->workers == NULL) {
+        err = errno;
+        goto fail_workers;
+    }
+
+    err = pthread_mutex_init(&p->mutex, NULL);
+    if (err)
+        goto fail_mutex;
+
+    err = pthread_cond_init(&p->not_empty, NULL);
+    if (err)
+        goto fail_not_empty;
+
+    for (unsigned i = 0; i < config->workers; i++) {
+        err = pthread_create(&p->workers[i], NULL, worker_thread, p);
+        if (err)
+            goto fail_thread;
+
+        p->workers_count++;
+    }
+
+    return 0;
+
+fail_thread:
+    stop_workers(p);
+    pthread_cond_destroy(&p->not_empty);
+fail_not_empty:
+    pthread_mutex_destroy(&p->mutex);
+fail_mutex:
+    free(p->workers);
+fail_workers:
+    free(p->queue);
+
+    return err;
+}
+
+static inline unsigned active_workers(struct hash_pool *p)
+{
+    return p->workers_count - p->workers_idle;
+}
+
+/*
+ * If the queue does not have enough work it is better to let idle workers
+ * sleep little bit to avoid waking up a worker for for every cycle.
+ */
+static inline bool need_wakeup(struct hash_pool *p)
+{
+    return p->workers_idle > 0 && p->queue_len > active_workers(p);
+}
+
+int hash_pool_submit(struct hash_pool *p, struct submission *sub)
+{
+    int err = 0;
+
+    mutex_lock(&p->mutex);
+
+    if (p->stopped) {
+        err = EPERM;
+        goto out;
+    }
+
+    if (p->error) {
+        err = p->error;
+        goto out;
+    }
+
+    /* The queue can never be full since the submitter waits until the
+     * submission queue is not full. */
+    if (p->queue_len == p->queue_size) {
+        err = ENOBUFS;
+        goto out;
+    }
+
+    push_unlocked(p, sub);
+
+    if (need_wakeup(p))
+        cond_signal(&p->not_empty);
+
+out:
+    mutex_unlock(&p->mutex);
+
+    if (err) {
+        submission_set_error(sub, err);
+        submission_complete(sub);
+    }
+
+    return err;
+}
+
+int hash_pool_destroy(struct hash_pool *p)
+{
+    stop_workers(p);
+
+    pthread_cond_destroy(&p->not_empty);
+    pthread_mutex_destroy(&p->mutex);
+    free(p->workers);
+    free(p->queue);
+
+    return 0;
+}
--- lib/submission.c
+++ lib/submission.c
@@ -1,11 +1,10 @@
 // SPDX-FileCopyrightText: Red Hat Inc
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
-#include <errno.h>
+#include <stdint.h>
 #include <stdlib.h>
-#include <string.h>
 
-#include "blkhash-internal.h"
+#include "submission.h"
 
 static int copy_data(struct submission *sub)
 {
@@ -21,12 +20,17 @@ static int copy_data(struct submission *sub)
     return 0;
 }
 
-int submission_init_data(struct submission *sub, struct stream *stream,
-                         int64_t index, uint32_t len, const void *data,
-                         struct completion *completion, uint8_t flags)
+int submission_init_data(struct submission *sub, int64_t index, uint32_t len,
+                         const void *data, struct completion *completion,
+                         uint8_t flags)
 {
+    int err = 0;
+
+    err = sem_init(&sub->sem, 0, 0);
+    if (err)
+        return errno;
+
     sub->type = DATA;
-    sub->stream = stream;
     sub->completion = completion;
     sub->data = data;
     sub->index = index;
@@ -35,24 +39,43 @@ int submission_init_data(struct submission *sub, struct stream *stream,
     sub->flags = flags;
 
     if (flags & SUBMIT_COPY_DATA) {
-        int err = copy_data(sub);
+        err = copy_data(sub);
         if (err)
-            return err;
+            goto error;
     }
 
     if (sub->completion)
         completion_ref(sub->completion);
 
     return 0;
+
+error:
+    if (sem_destroy(&sub->sem))
+        ABORTF("sem_destroy: %s", strerror(errno));
+
+    return err;
 }
 
-void submission_init_zero(struct submission *sub, struct stream *stream, int64_t index)
+int submission_init_zero(struct submission *sub, int64_t index)
 {
-    memset(sub, 0, sizeof(*sub));
+    int err;
+
+    /* Initialize to 1 so submission_is_completed() and submssion_wait() will
+     * return imediately. */
+    err = sem_init(&sub->sem, 0, 1);
+    if (err)
+        return errno;
 
     sub->type = ZERO;
-    sub->stream = stream;
     sub->index = index;
+
+    sub->data = NULL;
+    sub->completion = NULL;
+    sub->len = 0;
+    sub->error = 0;
+    sub->flags = 0;
+
+    return 0;
 }
 
 void submission_set_error(struct submission *sub, int error)
@@ -62,18 +85,105 @@ void submission_set_error(struct submission *sub, int error)
         completion_set_error(sub->completion, error);
 }
 
-int submission_error(struct submission *sub)
+int submission_error(const struct submission *sub)
 {
     return __atomic_load_n(&sub->error, __ATOMIC_ACQUIRE);
 }
 
 void submission_complete(struct submission *sub)
 {
+    if (sub->flags & SUBMIT_COPY_DATA) {
+        free((void *)sub->data);
+        sub->data = NULL;
+    }
+
+    if (sub->completion) {
+        completion_unref(sub->completion);
+        sub->completion = NULL;
+    }
+
+    /* Cannot fail in current code. */
+    if (sem_post(&sub->sem))
+        ABORTF("sem_post: %s", strerror(errno));
+}
+
+int submission_wait(struct submission *sub)
+{
+    int err;
+
+    do {
+        err = sem_wait(&sub->sem);
+    } while (err != 0 && errno == EINTR);
+
+    if (err != 0)
+        return errno;
+
+    return 0;
+}
+
+int submission_queue_pop(struct submission_queue *sq, struct submission **out)
+{
+    if (sq->len == 0)
+        return ENOENT;
+
+    if (out)
+        *out = &sq->ring[sq->head];
+
+    sq->head = (sq->head + 1) % sq->size;
+    sq->len--;
+
+    return 0;
+}
+
+int submission_queue_push(struct submission_queue *sq, struct submission **out)
+{
+    if (sq->len == sq->size)
+        return ENOBUFS;
+
+    *out = &sq->ring[sq->tail];
+
+    sq->tail = (sq->tail + 1) % sq->size;
+    sq->len++;
+
+    return 0;
+}
+
+void submission_destroy(struct submission *sub)
+{
+    /* If not completed, clean up now. */
     if (sub->flags & SUBMIT_COPY_DATA)
         free((void *)sub->data);
 
-    if (sub->completion)
-        completion_unref(sub->completion);
+    /* Cannot fail in current code. */
+    if (sem_destroy(&sub->sem))
+        ABORTF("sem_destroy: %s", strerror(errno));
 
     memset(sub, 0, sizeof(*sub));
+}
+
+int submission_queue_init(struct submission_queue *sq, unsigned size)
+{
+    sq->ring = malloc(size * sizeof(*sq->ring));
+    if (sq->ring == NULL)
+        return errno;
+
+    sq->size = size;
+    sq->len = 0;
+    sq->head = 0;
+    sq->tail = 0;
+
+    return 0;
+}
+
+void submission_queue_destroy(struct submission_queue *sq)
+{
+    struct submission *sub;
+
+    while (sq->len > 0) {
+        submission_queue_pop(sq, &sub);
+        submission_destroy(sub);
+    }
+
+    free(sq->ring);
+    memset(sq, 0, sizeof(*sq));
 }
