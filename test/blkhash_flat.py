460e3752137d690da61f97b0e5e2bd8dd3f8f42a
Nir Soffer <nsoffer@redhat.com>
2024-03-02 19:36:21 +0200

    Simplify reference implementation
    Remove multi-threading to keep the code simple as possible. The previous
    implementation was useful for prototyping but the purpose of the python
    version is to have a good way to verify the correctness of the actual
    optimized implementation.

--- test/blkhash_flat.py
+++ test/blkhash_flat.py
@@ -3,24 +3,20 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
 """
-blkhash parallel reference implementaion.
+blkhash reference implementaion.
 
-This is a simple and slow implementaion for verifying the C
-implementation. It does not use any optimzation and support only raw
-images.
+This is a trivial implemention not supporting zero optimization or
+multi-threading. It is usefful for verifying the C implemention.
 
 Usage:
 
-    blkhash.py [-h] [-d DIGEST] filename
+    blkhash.py [-h] [-d DIGEST_NAME] [-b BLOCK_SIZE] filename
 
 """
 
 import argparse
-import collections
 import hashlib
-import queue
 import struct
-import threading
 
 from functools import partial
 
@@ -29,9 +25,7 @@ from functools import partial
 BLOCK_SIZE = 64 * 1024
 
 # Values that do not affect te hash value.
-THREADS = 4
 READ_SIZE = 1024**2
-QUEUE_DEPTH = 32
 
 
 def main():
@@ -43,45 +37,22 @@ def main():
         help="Digest name (default sha256)",
     )
     p.add_argument(
-        "-t",
-        "--threads",
+        "-b",
+        "--block-size",
         type=int,
-        default=THREADS,
-        help=f"Number of threads (default {THREADS})",
-    )
-    p.add_argument(
-        "-q",
-        "--queue-depth",
-        type=int,
-        default=QUEUE_DEPTH,
-        help=f"Number of block to queue (default {QUEUE_DEPTH})",
+        default=BLOCK_SIZE,
+        help="Digest name (default sha256)",
     )
     p.add_argument("filename", help="Filename to checksum")
 
     args = p.parse_args()
-    blkhash = checksum(
-        args.filename,
-        args.digest_name,
-        threads=args.threads,
-        queue_depth=args.queue_depth,
-    )
+    blkhash = checksum(args.filename, args.digest_name, block_size=args.block_size)
     print(f"{blkhash}  {args.filename}")
 
 
-def checksum(
-    filename,
-    digest_name,
-    block_size=BLOCK_SIZE,
-    threads=THREADS,
-    queue_depth=QUEUE_DEPTH,
-):
+def checksum(filename, digest_name, block_size=BLOCK_SIZE):
     with open(filename, "rb") as f:
-        h = Blkhash(
-            digest_name,
-            block_size=block_size,
-            threads=threads,
-            queue_depth=queue_depth,
-        )
+        h = Blkhash(digest_name, block_size=block_size)
         for data in iter(partial(f.read, READ_SIZE), b""):
             h.update(data)
         return h.hexdigest()
@@ -93,35 +64,22 @@ class Blkhash:
 
     The hash is:
 
-        H( H(block 1) || H(block 2) ... H(block N) || length)
+        H( H(block 1) || ... || H(block N) || length)
 
     """
 
-    def __init__(
-        self,
-        digest_name,
-        block_size=BLOCK_SIZE,
-        threads=THREADS,
-        queue_depth=QUEUE_DEPTH,
-    ):
-        assert threads > 0
-
+    def __init__(self, digest_name, block_size=BLOCK_SIZE):
+        self.digest_name = digest_name
         self.block_size = block_size
-        self.queue_depth = queue_depth
-        self.hashpool = HasherPool(digest_name, threads)
-        self.futures = collections.deque()
-        self.root = hashlib.new(digest_name)
+        self.outer_hash = hashlib.new(digest_name)
         self.pending = None
         self.length = 0
         self.finalized = False
 
     def update(self, data):
-        if self.finalized:
-            raise RuntimeError("Hash finalized")
-
-        self.length += len(data)
-
+        assert not self.finalized
         with memoryview(data) as view:
+            # Consume pending data.
             if self.pending:
                 take = self.block_size - len(self.pending)
                 self.pending += view[:take]
@@ -130,26 +88,23 @@ class Blkhash:
                     self._add_block(self.pending)
                     self.pending = None
 
+            # Consume all full blocks.
             while len(view) >= self.block_size:
                 self._add_block(view[: self.block_size])
                 view = view[self.block_size :]
 
+            # Store partial block pending.
             if len(view):
                 self.pending = bytearray(view)
 
     def hexdigest(self):
         self._finalize()
-        return self.root.hexdigest()
+        return self.outer_hash.hexdigest()
 
     def _add_block(self, data):
-        if len(self.futures) == self.queue_depth:
-            block_digest = self.futures.popleft().result()
-            self.root.update(block_digest)
-        future = self.hashpool.hash(data)
-        self.futures.append(future)
-        while len(self.futures) and self.futures[0].done():
-            block_digest = self.futures.popleft().result()
-            self.root.update(block_digest)
+        block_hash = hashlib.new(self.digest_name, data).digest()
+        self.outer_hash.update(block_hash)
+        self.length += len(data)
 
     def _finalize(self):
         if self.finalized:
@@ -158,70 +113,8 @@ class Blkhash:
         if self.pending:
             self._add_block(self.pending)
             self.pending = None
-        while len(self.futures):
-            block_digest = self.futures.popleft().result()
-            self.root.update(block_digest)
-
         data = struct.pack("<Q", self.length)
-        self.root.update(data)
-
-        self.hashpool.stop()
-
-
-Work = collections.namedtuple("Work", "data,future")
-
-
-class HasherPool:
-    def __init__(self, digest_name, threads):
-        self.hashers = []
-        self.queue = queue.SimpleQueue()
-        for i in range(threads):
-            t = threading.Thread(
-                target=self._hasher,
-                args=(digest_name,),
-                name=f"blkhash/{i}",
-            )
-            t.start()
-            self.hashers.append(t)
-
-    def hash(self, data):
-        work = Work(data, Future())
-        self.queue.put(work)
-        return work.future
-
-    def stop(self):
-        for _ in self.hashers:
-            self.queue.put(None)
-        for t in self.hashers:
-            t.join()
-
-    def _hasher(self, digest_name):
-        hasher = hashlib.new(digest_name).copy
-        while True:
-            work = self.queue.get()
-            if work is None:
-                break
-            h = hasher()
-            h.update(work.data)
-            work.future.set_result(h.digest())
-            work = None
-
-
-class Future:
-    def __init__(self):
-        self._result = None
-        self._done = threading.Event()
-
-    def set_result(self, result):
-        self._result = result
-        self._done.set()
-
-    def done(self):
-        return self._done.is_set()
-
-    def result(self):
-        self._done.wait()
-        return self._result
+        self.outer_hash.update(data)
 
 
 if __name__ == "__main__":
